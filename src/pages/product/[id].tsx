89ca18b8397e69efebf3ce92157e2a04ecbfa675
GabrielFerlima <gabrielpanii@gmail.com>
2022-12-22 14:51:08 -0300

    criando checkout ao comprar produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,12 +12,17 @@ interface ProductProps {
         imageUrl: string
         price: string
         description: string
+        defaultPriceId: string
     }
 }
 
 export default function Product({ product }: ProductProps) {
+
+    function handleBuyProduct() {
+        console.log(product.defaultPriceId)
+    }
     const { isFallback } = useRouter()
-    
+
     if (isFallback) {
         return <p>Loading...</p>
     }
@@ -32,7 +37,7 @@ export default function Product({ product }: ProductProps) {
                 <h1>{product.name}</h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button>
+                <button onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDetails>
@@ -72,6 +77,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                     currency: 'BRL',
                 }).format(price.unit_amount / 100),
                 description: product.description,
+                defaultPriceId: price.id,
             }
         },
         revalidate: 60 * 60 * 1 // 1 hour
