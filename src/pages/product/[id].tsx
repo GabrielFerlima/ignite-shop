30a2b22a3057bab9162b4a9a355fdba1dc06e72c
GabrielFerlima <gabrielpanii@gmail.com>
2022-12-25 15:06:56 -0300

    adicionado botao de comprar levando as rotas dos produtos para checkout no stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Stripe from "stripe"
 import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
@@ -18,8 +20,26 @@ interface ProductProps {
 
 export default function Product({ product }: ProductProps) {
 
-    function handleBuyProduct() {
-        console.log(product.defaultPriceId)
+    const [isCreatingCheckoutSession, setCreatingCheckoutSession] = useState(false)
+
+    async function handleBuyProduct() {
+        try {
+            setCreatingCheckoutSession(true)
+
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+
+            const {checkoutUrl} = response.data
+
+            window.location.href = checkoutUrl
+        } catch(err) {
+            // Conectar a uma ferramenta de observabilidade (Datalog / Sentry)
+
+            setCreatingCheckoutSession(false)
+
+            alert('Falha ao redirecionar ao checkout!')
+        }
     }
     const { isFallback } = useRouter()
 
@@ -37,7 +57,7 @@ export default function Product({ product }: ProductProps) {
                 <h1>{product.name}</h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button onClick={handleBuyProduct}>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDetails>
